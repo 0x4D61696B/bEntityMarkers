860191cfef05f80f3e8da4d104d556706d140746
0x4D61696B <365945ba@gmail.com>
2015-08-11 04:38:40 +0200

    Replaced tabs with spaces

--- bEntityMarkers.lua
+++ bEntityMarkers.lua
@@ -24,46 +24,46 @@ Debug.EnableLogging(false)
 -- =============================================================================
 
 function Notification(message)
-	ChatLib.Notification({text = "[bEntityMarkers] " .. tostring(message)})
+    ChatLib.Notification({text = "[bEntityMarkers] " .. tostring(message)})
 end
 
 function LogEntityInfo()
-	local reticleInfo = Player.GetReticleInfo()
-	
-	if (reticleInfo.entityId) then
-		local entityInfo = Game.GetTargetInfo(reticleInfo.entityId)
-		
-		if (entityInfo) then
-			log(tostring(entityInfo))
-		end
-	end
+    local reticleInfo = Player.GetReticleInfo()
+    
+    if (reticleInfo.entityId) then
+        local entityInfo = Game.GetTargetInfo(reticleInfo.entityId)
+        
+        if (entityInfo) then
+            log(tostring(entityInfo))
+        end
+    end
 end
 
 function OnPlayerMenuShow(playerName, reason)
-	if (not namecompare(playerName, Player.GetInfo())) then
-		local MENU = PlayerMenu:AddMenu({label = "bEntityMarkers", menu = "bEntityMarkers_menu"})
-		local toggleLabel = function(pName) if (Options.IO.Character.Name.List[pName]) then return "Remove from" else return "Add to" end end
-		
-		MENU:AddButton({label = toggleLabel(playerName) .. " tracking list", id = "bEntityMarkers_toggle"}, function()
-			Options.AddRemovePlayerName(playerName)
-		end)
-	end
+    if (not namecompare(playerName, Player.GetInfo())) then
+        local MENU = PlayerMenu:AddMenu({label = "bEntityMarkers", menu = "bEntityMarkers_menu"})
+        local toggleLabel = function(pName) if (Options.IO.Character.Name.List[pName]) then return "Remove from" else return "Add to" end end
+        
+        MENU:AddButton({label = toggleLabel(playerName) .. " tracking list", id = "bEntityMarkers_toggle"}, function()
+            Options.AddRemovePlayerName(playerName)
+        end)
+    end
 end
 
 function OnSlashCommand(args)
-	if (args[1]) then
-		if (args[1] == "-clear") then
-			Options.ClearPlayerNames()
-		elseif (args[1] == "-info") then
-			LogEntityInfo()
-		elseif (args[1] == "-list") then
-			Options.ListPlayerNames()
-		elseif (unicode.len(args[1]) > 0) then
-			Options.AddRemovePlayerName(args[1])
-		end
-	else
-		Notification("/bem <-clear|-list|player_name>\n\t-clear : Clears all currently tracked character names\n\t-list : Lists all currently tracked player names\n\tplayer_name - Adds/removes the specified player name to/from the tracking list")
-	end
+    if (args[1]) then
+        if (args[1] == "-clear") then
+            Options.ClearPlayerNames()
+        elseif (args[1] == "-info") then
+            LogEntityInfo()
+        elseif (args[1] == "-list") then
+            Options.ListPlayerNames()
+        elseif (unicode.len(args[1]) > 0) then
+            Options.AddRemovePlayerName(args[1])
+        end
+    else
+        Notification("/bem <-clear|-list|player_name>\n\t-clear : Clears all currently tracked character names\n\t-list : Lists all currently tracked player names\n\tplayer_name - Adds/removes the specified player name to/from the tracking list")
+    end
 end
 
 
@@ -72,41 +72,41 @@ end
 -- =============================================================================
 
 function OnComponentLoad()
-	LIB_SLASH.BindCallback({
-		slash_list = "bentitymarkers, bem",
-		description = "bEntityMarkers",
-		func = OnSlashCommand,
-		autocomplete_name = 1
-	})
-	Options.Setup()
-	PlayerMenu.BindOnShow(OnPlayerMenuShow)
+    LIB_SLASH.BindCallback({
+        slash_list = "bentitymarkers, bem",
+        description = "bEntityMarkers",
+        func = OnSlashCommand,
+        autocomplete_name = 1
+    })
+    Options.Setup()
+    PlayerMenu.BindOnShow(OnPlayerMenuShow)
 end
 
 function OnEnterExitZone()
-	Tracker.ClearMapMarkers()
+    Tracker.ClearMapMarkers()
 end
 
 function OnDeployableRosterUpdate(args)
-	if (args.entityId) then
-		Debug.Log("OnDeployableRosterUpdate()", args)
-		Tracker.UpdateDeployableRoster(args)
-	end
+    if (args.entityId) then
+        Debug.Log("OnDeployableRosterUpdate()", args)
+        Tracker.UpdateDeployableRoster(args)
+    end
 end
 
 function OnEntityAvailable(args)
-	if (Options.IO.General.Enable) then
-		Callback2.FireAndForget(Tracker.CheckEntity, args.entityId, Options.IO.General.TrackingDelay)
-	end
+    if (Options.IO.General.Enable) then
+        Callback2.FireAndForget(Tracker.CheckEntity, args.entityId, Options.IO.General.TrackingDelay)
+    end
 end
 
 function OnEntityLost(args)
-	Tracker.RemoveMapMarker(args.entityId)
+    Tracker.RemoveMapMarker(args.entityId)
 end
 
 function OnPlayerReady()
-	Tracker.Setup()
+    Tracker.Setup()
 end
 
 function OnSinCardOp(args)
-	-- TODO: implement surface deposit tracking?
+    -- TODO: implement surface deposit tracking?
 end
